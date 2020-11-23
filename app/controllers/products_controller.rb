e03eaedeb227435b2f797e51dedba4803d535ed6
beatriz andre <biacaram@hotmail.com>
2020-11-23 17:31:57 -0200

    create ProductsController

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,49 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @products = Product.all
+  end
+
+  def show; end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @product = Product.new(product_params)
+    @product.user = @user
+
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @product.update(product_params)
+    redirect_to product_path(@product)
+  end
+
+  def destroy 
+    @user = @product.user
+    @product.destroy
+    redirect_to @user
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+  
+end
