4c8b0d1f84c69dfe1934b612656773b5e8e68e58
beatriz andre <biacaram@hotmail.com>
2020-11-24 15:18:29 -0200

    correção da route destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: %i[new create] do
-    resources :products, only: %i[new create edit update]
+    resources :products, only: %i[new create edit update destroy]
   end
-  resources :products, only: %i[index show destroy]
+  resources :products, only: %i[index show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
