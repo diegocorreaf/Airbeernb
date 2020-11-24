6f720464f081491dbec9107d66873b1b0fac9ea7
beatriz andre <biacaram@hotmail.com>
2020-11-24 15:58:46 -0200

    add before_action e skip_before_action no controller de products

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: %i[new create] do
-    resources :products, only: %i[new create edit update destroy]
-  end
-  resources :products, only: %i[index show]
+  resources :products, shallow: true
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
