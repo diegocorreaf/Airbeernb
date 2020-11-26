c011a2da385d6631fb9732ae2ac8eac2cd113044
diego fernandes <diegoc.fernandes1@gmail.com>
2020-11-26 11:44:54 -0300

    Product has address now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_143904) do
+ActiveRecord::Schema.define(version: 2020_11_26_144409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_143904) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price"
     t.bigint "user_id", null: false
+    t.string "address"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
