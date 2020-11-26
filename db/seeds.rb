eebc2b5a76cc80ba107d447809370dbe85badbc2
beatriz andre <biacaram@hotmail.com>
2020-11-26 17:16:03 -0200

    font-family, index-btn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  Product.create!(name: Faker::Beer.name,
+                  details: Faker::Beer.style,
+                  price: rand(1..20),
+                  photo: Faker::Name.name)
+end
