cdb3e45e029fc01d713298406e214f5cc76287e6
diego fernandes <diegoc.fernandes1@gmail.com>
2020-11-27 16:06:14 -0300

    import restored

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,9 +21,6 @@ require("channels")
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
-import "plugins/popper";
-import "plugins/bootstrap";
-import "plugins/jquery";
 
 // External imports
 import "bootstrap";
